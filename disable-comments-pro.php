0f9dc39b0ab8f2354f425eef1adb1d9db371eebd
Youssef E. Amer <102834473+joexamer@users.noreply.github.com>
2025-06-20 01:00:11 +0300

    Update disable-comments-pro.php

--- disable-comments-pro.php
+++ disable-comments-pro.php
@@ -10,7 +10,6 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -42,7 +41,6 @@ function dcp_disable_backend_comments() {
         wp_die( __( 'Comments are disabled site-wide.', 'disable-comments-pro' ), '', array( 'response' => 403 ) );
     }
 
-    add_action( 'admin_menu', 'dcp_remove_comment_menu' );
     add_action( 'add_meta_boxes', 'dcp_remove_comment_meta_boxes' );
     add_action( 'admin_init', 'dcp_remove_comment_support' );
 }
@@ -55,6 +53,7 @@ function dcp_remove_comment_menu() {
         remove_menu_page( 'edit-comments.php' );
     }
 }
+add_action( 'admin_menu', 'dcp_remove_comment_menu', 99 );
 
 function dcp_remove_comment_meta_boxes() {
     $options = get_option( 'dcp_settings' );
@@ -81,13 +80,14 @@ function dcp_remove_comment_support() {
         }
     }
 }
+
 function dcp_add_settings_page() {
     add_options_page(
         'Disable Comments Pro Settings',
         'Disable Comments Pro',
         'manage_options',
         'disable-comments-pro',
-        'dcp_settings_page_content' 
+        'dcp_settings_page_content'
     );
 }
 add_action( 'admin_menu', 'dcp_add_settings_page' );
@@ -102,7 +102,7 @@ function dcp_settings_init() {
     add_settings_section(
         'dcp_settings_section',
         'Plugin Settings',
-        'dcp_settings_section_callback',
+        'dcp_settings_section_callback', 
         'disable-comments-pro'
     );
 
@@ -125,17 +125,17 @@ function dcp_settings_init() {
 add_action( 'admin_init', 'dcp_settings_init' );
 
 function dcp_settings_section_callback() {
-    echo '&lt;p&gt;Configure the settings for the Disable Comments Pro plugin.&lt;/p&gt;';
+    echo '<p>Configure the settings for the Disable Comments Pro plugin.</p>';
 }
 
 function dcp_enabled_field_callback() {
     $options = get_option( 'dcp_settings' );
     $enabled = isset( $options['enabled'] ) ? (bool) $options['enabled'] : false;
     ?>
-    &lt;label for="dcp_enabled"&gt;
-        &lt;input type="checkbox" name="dcp_settings[enabled]" id="dcp_enabled" value="1" &lt;?php checked( $enabled, true ); ?&gt;&gt;
+    <label for="dcp_enabled">
+        <input type="checkbox" name="dcp_settings[enabled]" id="dcp_enabled" value="1" <?php checked( $enabled, true ); ?>>
         Check this box to enable the plugin.
-    &lt;/label&gt;
+    </label>
     <?php
 }
 
@@ -144,15 +144,15 @@ function dcp_post_types_field_callback() {
     $selected_post_types = isset( $options['post_types'] ) ? (array) $options['post_types'] : array();
     $post_types = get_post_types( array( 'public' => true ), 'objects' );
 
-    echo '&lt;p&gt;Select the post types for which comments should be disabled:&lt;/p&gt;';
-    echo '&lt;ul&gt;';
+    echo '<p>Select the post types for which comments should be disabled:</p>';
+    echo '<ul>';
     foreach ( $post_types as $post_type_object ) {
         $post_type = $post_type_object->name;
         $label = $post_type_object->labels->singular_name;
         $checked = in_array( $post_type, $selected_post_types ) ? 'checked' : '';
-        echo "&lt;li&gt;&lt;label&gt;&lt;input type='checkbox' name='dcp_settings[post_types][]' value='{$post_type}' {$checked}&gt; {$label}&lt;/label&gt;&lt;/li&gt;";
+        echo "<li><label><input type='checkbox' name='dcp_settings[post_types][]' value='{$post_type}' {$checked}> {$label}</label></li>";
     }
-    echo '&lt;/ul&gt;';
+    echo '</ul>';
 }
 
 function dcp_sanitize_settings( $input ) {
@@ -171,15 +171,15 @@ function dcp_sanitize_settings( $input ) {
 
 function dcp_settings_page_content() {
     ?>
-    &lt;div class="wrap"&gt;
-        &lt;h1&gt;Disable Comments Pro Settings&lt;/h1&gt;
-        &lt;form method="post" action="options.php"&gt;
-            &lt;?php
+    <div class="wrap">
+        <h1>Disable Comments Pro Settings</h1>
+        <form method="post" action="options.php">
+            <?php
             settings_fields( 'dcp_settings_group' );
             do_settings_sections( 'disable-comments-pro' );
             submit_button();
-            ?&gt;
-        &lt;/form&gt;
-    &lt;/div&gt;
+            ?>
+        </form>
+    </div>
     <?php
 }
